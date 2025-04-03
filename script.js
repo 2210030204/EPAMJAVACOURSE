e8c3448ba9123fc07e680bda773adfc2a03e7972
2210030204 <153171746+2210030204@users.noreply.github.com>
2025-04-03 11:35:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import profileImage from './assets/logo.jpg';
+
+function App() {
+  return (
+    <div>
+      <h1>K L University</h1>
+      <img src={profileImage} alt="KL University Logo" />
+    </div>
+  );
+}
+
+export default App;
